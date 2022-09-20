83f682260846fbcd39250ff492ac2f59d460c7c7
rezwanahmodsami <samiahmed0f0@gmail.com>
2022-09-21 01:16:40 +0600

    Jquery dependency removed, now routemamba is a standalone library.

--- example/content/home.php
+++ example/content/home.php
@@ -1,1 +1,4 @@
 <h1 style="color: blue;">This home path  </h1>
+<script>console.log("script is working")</script>
+<script>console.log("script is working 2")</script>
+<script src="./s.js"></script>
