f48c8c20307def477152a5c4ce89b0a568f8b4e5
rezwanahmodsami <samiahmed0f0@gmail.com>
2022-11-08 00:11:52 +0600

    TabConfig, TabHandler and TabRegister types added as types

--- src/types.ts
+++ src/types.ts
@@ -2,15 +2,19 @@
 /**
  * types of every route method objects @param
  */
-type RouteMethod            = string;
-type RouteMetaLoader        = boolean;
-type RouteContentUrl        = string;
-type RouteContainer         = string;
-type RoutePreloader         = string;
-type RouteData              = object;
-type RouteErrorContent      = string;
-type RouteHttpUrlChange     = boolean;
-type RouteHttpUrl           = string;
+export type RouteMethod            = string;
+export type RouteMetaLoader        = boolean;
+export type RouteContentUrl        = string;
+export type RouteContainer         = string;
+export type RoutePreloader         = string;
+export type RouteData              = object;
+export type RouteErrorContent      = string;
+export type RouteHttpUrlChange     = boolean;
+export type RouteHttpUrl           = string;
+export type RouteMetaUrl           = string;
+export type RouteDomContent        = string;
+export type RouteServerHost        = string;
+export type RouteErrorHead         = string;
 
 
 /**
@@ -44,3 +48,23 @@ export type NavigationOptions  = {
     header_load?:       NavigationHeaderLoad,
     footer_load?:       NavigationFooterLoad
 }
+
+/**
+ * Tab handler Options types @param
+ */
+export type TabConfig = {
+    tabRoute:           string,
+    content_url:        string,
+    preloader:          string,
+    error_handler:      string
+}
+
+export type TabHandler = {
+    tabSwitcher:            string,
+    initialTab:             string,
+    activeSwitcherClass:    string,
+    activeTabClass:         string,
+    tabs:                   Array<TabConfig>
+}
+
+export type TabRegister = Array<TabHandler>;
