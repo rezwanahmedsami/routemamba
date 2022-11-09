b47757384b9940c9ddbd47a07ca1456af2fdb619
rezwanahmodsami <samiahmed0f0@gmail.com>
2022-11-09 15:06:27 +0600

    Rmvalidator added

---
+++ src/RmValidator.ts
@@ -0,0 +1,9 @@
+/** there all validators @functions exporting for validate string, objects, arrays etc from one place  */
+
+import isValidUrl from "./validators/isValidUrl"
+
+const RmValidator = {
+    isValidUrl,
+}
+
+export default RmValidator
--- src/routemamba.ts
+++ src/routemamba.ts
@@ -1,9 +1,14 @@
 import { __render_DOM } from "./DomRenderer";
+import RmValidator from "./RmValidator";
 
 let scripContent = "<script>console.log('loaded from load script')";
 
+const testingValidator = () => {
+    return RmValidator.isValidUrl("https://www.google.com");
+}
 
 export {
     __render_DOM,
     scripContent,
+    testingValidator
 }
