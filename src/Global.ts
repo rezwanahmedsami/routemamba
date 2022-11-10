254ce287630f9cde6d98d36ad4f96006bd13d538
rezwanahmodsami <samiahmed0f0@gmail.com>
2022-11-10 14:53:54 +0600

    persist storage added

--- src/Global.ts
+++ src/Global.ts
@@ -1,4 +1,4 @@
-import { RegisterdRoutesFooters, RegisterdRoutesHeaders, RegisterdRoutesPages } from "./types";
+import { RegisterdRoutesFooters, RegisterdRoutesHeaders, RegisterdRoutesPages, RouteErrorHead, RouteMetaUrl, RouteServerHost } from "./types";
 
 /**
  * there all golbal array, object and variable initialized
@@ -7,4 +7,20 @@ export namespace  RoutesStorage {
     export var RoutesPages:             RegisterdRoutesPages = [];
     export var RoutesHeaders:           RegisterdRoutesHeaders<Array<string>> = [];
     export var RoutesFooters:           RegisterdRoutesFooters<Array<string>> = [];
+    export var server_host:             RouteServerHost = "";
+    export var meta_content_url:        RouteMetaUrl = "";
+}
+
+/**
+ * this is persist storage to store readonly value.
+ */
+export namespace PersistStorage {
+
+    interface persistantDomContent{
+        readonly ErrorHeadContent:      RouteErrorHead;
+    }
+
+    export var DomContent:              persistantDomContent = {
+        ErrorHeadContent: `<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>ERROR</title>`,
+    };
 }
--- src/routemamba.ts
+++ src/routemamba.ts
@@ -1,5 +1,5 @@
 import { __render_DOM } from "./DomRenderer";
-// import { RoutesStorage } from "./Global";
+import { PersistStorage, RoutesStorage } from "./Global";
 import RmValidator from "./RmValidator";
 
 let scripContent = "<script>console.log('loaded from load script')";
@@ -7,6 +7,8 @@ let scripContent = "<script>console.log('loaded from load script')";
 const testingValidator = () => {
     return RmValidator.isValidUrl("https://www.google.com");
 }
+
+// PersistStorage.DomContent.ErrorHeadContent = "adfsd"
 
 export {
     __render_DOM,
